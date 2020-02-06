665c830d5d61d614bd343916f6e1952eef2c04c5
Gregiorg <gergely.fodor@etu.univ-grenoble-alpes.fr>
2020-02-06 09:58:10 +0100

    etape 4, redirection entree sortie

--- src/shell.c
+++ src/shell.c
@@ -7,6 +7,10 @@
 #include "readcmd.h"
 #include "csapp.h"
 
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
 int main()
 {
 	while (1) {
@@ -27,9 +31,6 @@ int main()
 			continue;
 		}
 
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-
 		/* Display each command of the pipe */
 		// int i, j;
 		// for (i = 0; l->seq[i] != 0; i++) {
@@ -45,6 +46,31 @@ int main()
 			printf("Quitting the shell ...\n");
 			printf("Hope you'll come back soon master ... UwU\n");
 			exit(0);
+		} else {
+
+			pid_t pid;
+			if ((pid = Fork()) == 0) { // child process
+				int inDesc, outDesc;
+
+				if (l->in) {
+					inDesc = Open(l->in, O_RDONLY, 0);
+					Dup2(inDesc, STDIN);
+				}
+
+				if (l->out) {
+					mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+					outDesc = Open(l->out, O_CREAT | O_WRONLY, mode);
+					Dup2(outDesc, STDOUT);
+				}
+
+				if (execvp(l->seq[0][0], l->seq[0]) == -1) {
+					perror("Command error");
+					exit(-1);
+				}
+
+			} else { // parent process
+				Wait(0);
+			}
 		}
 	}
 }
